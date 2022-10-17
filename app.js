319786bfce4d24880283f1a94f09d5c25040f8bd
Hamzaansari <hamzaansaree15@gmail.com>
2022-10-18 04:00:08 +0500

    Update JS File

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-let search = () => {
-    let searchBtn = document.getElementById('search_btn');
+let searchBtn = document.getElementById('search_btn');
+let closeBtn = document.getElementById('close_btn');
+let searchBar = document.getElementById('search_bar');
+let menuToggle = document.getElementById('menu');
+let navigation = document.getElementById('navigation');
+let header = document.getElementById('header');
+searchBtn.addEventListener('click', () => {
+    searchBar.classList.add('active');
+    closeBtn.classList.add('active');
     searchBtn.classList.add('active');
-}
+    menuToggle.classList.add('active');
+});
+closeBtn.addEventListener('click', () => {
+    searchBar.classList.remove('active');
+    closeBtn.classList.remove('active');
+    searchBtn.classList.remove('active');
+    menuToggle.classList.remove('active');
+});
+menuToggle.addEventListener('click', () => {
+    header.classList.toggle('show')
+});
