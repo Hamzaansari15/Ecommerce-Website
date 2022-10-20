4df2d415a94479a0dc23c7233b90d9d7a59e03a3
Hamzaansari <hamzaansaree15@gmail.com>
2022-10-21 02:22:02 +0500

    Update JS File

--- app.js
+++ app.js
@@ -19,3 +19,37 @@ closeBtn.addEventListener('click', () => {
 menuToggle.addEventListener('click', () => {
     header.classList.toggle('show')
 });
+// const getData = () => {
+//     let url = 'https://fakestoreapi.com/products'
+//     fetch(url)
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         for(let i = 0; i < data.length; i++){
+//             console.log(data[i]);
+//         }
+//     })
+// } 
+// getData();
+fetch('https://fakestoreapi.com/products/categories')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+fetch("https://fakestoreapi.com/products/category/women's clothing")
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+            fetch('https://fakestoreapi.com/products',{
+            method:"POST",
+            body:JSON.stringify(
+                {
+                    title: 'test product',
+                    price: 13.5,
+                    description: 'lorem ipsum set',
+                    image: 'https://i.pravatar.cc',
+                    category: 'electronic'
+                }
+            )
+        })
+            .then(res=>res.json())
+            .then(json=>console.log(json))
