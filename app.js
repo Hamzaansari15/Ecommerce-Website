22488ba883cb83397fecf74d4fadb9d5c008a13e
Hamzaansari <hamzaansaree15@gmail.com>
2022-10-22 04:39:07 +0500

    Update JS File

--- app.js
+++ app.js
@@ -33,23 +33,32 @@ menuToggle.addEventListener('click', () => {
 //     })
 // } 
 // getData();
-fetch('https://fakestoreapi.com/products/categories')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-fetch("https://fakestoreapi.com/products/category/women's clothing")
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-            fetch('https://fakestoreapi.com/products',{
-            method:"POST",
-            body:JSON.stringify(
-                {
-                    title: 'test product',
-                    price: 13.5,
-                    description: 'lorem ipsum set',
-                    image: 'https://i.pravatar.cc',
-                    category: 'electronic'
-                }
-            )
-        })
-            .then(res=>res.json())
-            .then(json=>console.log(json))
+// fetch('https://fakestoreapi.com/products/categories')
+//             .then(res=>res.json())
+//             .then(json=>console.log(json))
+// fetch("https://fakestoreapi.com/products/category/women's clothing")
+//             .then(res=>res.json())
+//             .then(json=>console.log(json))
+//             fetch('https://fakestoreapi.com/products',{
+//             method:"POST",
+//             body:JSON.stringify(
+//                 {
+//                     title: 'test product',
+//                     price: 13.5,
+//                     description: 'lorem ipsum set',
+//                     image: 'https://i.pravatar.cc',
+//                     category: 'electronic'
+//                 }
+//             )
+//         })
+//             .then(res=>res.json())
+//             .then(json=>console.log(json))
+const getData = () => {
+    let url = 'https://api.storerestapi.com/categories';
+    fetch(url).then((Response) => {
+        return Response.json()
+    }).then((data) => {
+        console.log(data);
+    })
+}
+getData()
