5ec4a385d5c999eee1ea17126ad01779369cccd3
Hamzaansari <hamzaansaree15@gmail.com>
2022-10-25 16:32:02 +0500

    Fetching Data from API

--- app.js
+++ app.js
@@ -19,46 +19,19 @@ closeBtn.addEventListener('click', () => {
 menuToggle.addEventListener('click', () => {
     header.classList.toggle('show')
 });
-// const getData = () => {
-//     let url = 'https://fakestoreapi.com/products'
-//     fetch(url)
-//     .then((response) => {
-//         return response.json()
-//     })
-//     .then((data) => {
-//         console.log(data);
-//         for(let i = 0; i < data.length; i++){
-//             console.log(data[i]);
-//         }
-//     })
-// } 
-// getData();
-// fetch('https://fakestoreapi.com/products/categories')
-//             .then(res=>res.json())
-//             .then(json=>console.log(json))
-// fetch("https://fakestoreapi.com/products/category/women's clothing")
-//             .then(res=>res.json())
-//             .then(json=>console.log(json))
-//             fetch('https://fakestoreapi.com/products',{
-//             method:"POST",
-//             body:JSON.stringify(
-//                 {
-//                     title: 'test product',
-//                     price: 13.5,
-//                     description: 'lorem ipsum set',
-//                     image: 'https://i.pravatar.cc',
-//                     category: 'electronic'
-//                 }
-//             )
-//         })
-//             .then(res=>res.json())
-//             .then(json=>console.log(json))
 const getData = () => {
-    let url = 'https://api.storerestapi.com/categories';
-    fetch(url).then((Response) => {
-        return Response.json()
-    }).then((data) => {
-        console.log(data);
-    })
-}
+    let url = 'https://fakestoreapi.com/products/category/jewelery'
+    fetch(url)
+        .then(Response => {
+            return Response.json();
+        })
+        .then(data => {
+            console.log(data);
+            for(let i = 0; i < data.length; i++){
+                // console.log(data[i]);
+                console.log(data[i].title);
+                let title = data[i].title;
+            }
+        })
+} 
 getData()
