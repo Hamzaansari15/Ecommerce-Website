dc69de01a405791e1e2111821ad64e57baf9dc70
Hamzaansari <hamzaansaree15@gmail.com>
2022-10-30 05:17:55 +0500

    Update JS File

--- app.js
+++ app.js
@@ -19,45 +19,52 @@ closeBtn.addEventListener('click', () => {
 menuToggle.addEventListener('click', () => {
     header.classList.toggle('show')
 });
+const getDate = () => {
+    let year = new Date().getFullYear();
+let date = document.getElementById('date');
+date.innerHTML = year;
+};
+getDate();
 const getData = () => {
-    let url = 'https://fakestoreapi.com/products/category/jewelery'
+    let url = 'https://fakestoreapi.com/products'
     fetch(url)
         .then(Response => {
             return Response.json();
         })
         .then(data => {
-            // console.log(data);
-            for(let i = 0; i < data.length; i++){
+            for (let i = 0; i < data.length; i++) {
                 let image = data[i].image;
-                console.log(data[i].title);
-                let title = data[i].title;
-                if(true){
-                    console.log('hamza');
-                    
-                }
-                console.log(data[i].category);
+                let title = data[i].title.split(' ').slice(0,4).join(' ');
                 let category = data[i].category;
-                console.log(data[i].price);
                 let price = data[i].price;
-                let discountPrice = price/10;
-                let discount = price - discountPrice;
-                console.log(data[i].rating);
-                let ratings = data[i].rating; 
-                console.log(ratings.rate);
-                let rate = ratings.rate;      
+                let discountPrice = price / 10;
+                let discount = (price - discountPrice).toFixed();
+                let ratings = data[i].rating;
+                let rate = ratings.rate;
                 let allCard = document.getElementById('all_card');
                 let card = `<div id="card">
-                <div id="card_image"><img id="image" src=${image} alt=""
-                    srcset=""></div>
-                    <div id="description">
-                      <p id="title">${title}</p>
-                      <p id="category">category : ${category}</p>
-                      <p id="price">Price : <s>${price}</s>  ${discount}</p>
-                      <p id="rating">Rating : ${rate}</p>
-                    </div>
-              </div>`;
-              allCard.innerHTML += card;
+                               <div id="card_image"><img id="image" src=${image} alt=""srcset=""></div>
+                               <div id="description">
+                                    <p id="title">${title}</p>
+                                    <p id="category">category : ${category}</p>
+                                    <p id="price">Price : <del>${price}$</del>  ${discount}$</p>
+                                    <p id="rating">Rating : ${rate}</p>
+                               </div>
+                            </div>`;
+                allCard.innerHTML += card;
             }
         })
-} 
-getData()
+}
+getData();
+
+// Another Fetching DATA
+
+
+let url = 'https://api.escuelajs.co/api/v1/products';
+fetch(url)
+.then((Response) => {
+    return Response.json();
+})
+.then((data) => {
+    console.log(data);
+})
