1d8a8107098cfef8884e26921fdc7f910a086d7a
Hamzaansari <hamzaansaree15@gmail.com>
2022-11-07 23:42:54 +0500

    Update Product Page

--- product/product.js
+++ product/product.js
@@ -7,28 +7,20 @@ fetch(url)
 .then((data) => {
     console.log(data);
     for(let i = 0; i <= 100; i++){
-        console.log(data[i]);
-        // console.log(data[i].description);
-        let description = data[i].description;
-        // console.log(data[i].category.name);
         let category = data[i].category.name;
-        // console.log(data[i].images);
         let image = data[i].images;
-        for(let j = 3; j <= 3; j++){
-            console.log(image[j]);
-        }
-        // console.log(data[i].title);
         let title = data[i].title;
-        // console.log(data[i].price);
         let price = data[i].price;
         let allCard = document.getElementById('all_card');
-        let card = `<div onclick="showData()" id="card">
+        let card = `<div id="card">
                                <div id="card_image"><img id="image" src=${image} alt=""srcset=""></div>
                                <div id="description">
                                     <p id="title">${title}</p>
                                     <p id="category">category :<span> ${category}</span></p>
                                     <p id="price">Price : <span>${price}$</span></p>
                                     <p id="rating">Rating : <span> 3.9</span></p>
+                                    <div id="cart_btn"><button><i class="fa-solid fa-cart-shopping"></i><span class="icon_text">Add to
+                                    cart</span></button></div>
                                </div>
                             </div>`;
                 allCard.innerHTML += card;
